ed96dd5e981eca523a46e64e6102bbb7c9c642e8
juxsalley <juxsalley@gmail.com>
2021-03-11 17:15:03 +0000

    edit readme.md

--- app/controllers/home_controller.rb
+++
@@ -1,7 +0,0 @@
-class HomeController < ApplicationController 
-    before_action :authenticate_user!
-    def index
-        @past_events = Event.past
-        @upcoming_events = Event.upcoming
-    end
-end
