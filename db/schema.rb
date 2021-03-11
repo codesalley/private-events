7d3d9233e53aac8fbd846a12443c740ab5c5199f
juxsalley <juxsalley@gmail.com>
2021-03-11 09:15:18 +0000

    add invite model to connect user to events

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_09_221313) do
+ActiveRecord::Schema.define(version: 2021_03_11_091443) do
 
   create_table "attendees", force: :cascade do |t|
     t.string "full_name"
@@ -28,6 +28,13 @@ ActiveRecord::Schema.define(version: 2021_03_09_221313) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "event_creator_id"
+  end
+
+  create_table "invites", force: :cascade do |t|
+    t.integer "attended_event_id"
+    t.integer "attendee_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "users", force: :cascade do |t|
